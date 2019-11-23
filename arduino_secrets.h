a33f82bb1672d005f1e4cc733911b4c66c7b7b21
VictorMoraJimenez <36267572+VictorMoraJimenez@users.noreply.github.com>
2019-11-23 20:24:39 +0100

    Oriol Rius code

---
+++ arduino_secrets.h
@@ -0,0 +1,8 @@
+//Wifi config
+#define SECRET_SSID "WrpiVictorMora"
+#define SECRET_PASS "Mirvic11."
+//MQTT config
+#define SECRET_MQTT_HOST "192.168.1.90"
+#define SECRET_MQTT_USER "vmoraji"
+#define SECRET_MQTT_PASS "Curs-IoT.19"
+#define SECRET_MQTT_CLIENT_ID "device001"
